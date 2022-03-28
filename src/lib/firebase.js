1f0be616c02190dd8d2d98f4565d5886ee30a52b
morshaked88 <morshaked@gmail.com>
2022-03-28 23:08:47 +0300

    login setup tailwind

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,24 +1,19 @@
-import Firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-
-//import seed file
-import { seedDatabase } from "../seed";
+import Firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
-  apiKey: "AIzaSyCU1CkfC-zag_0jTDr6wdwkbGtKJuoDWms",
-  authDomain: "homies-d82f4.firebaseapp.com",
-  projectId: "homies-d82f4",
-  storageBucket: "homies-d82f4.appspot.com",
-  messagingSenderId: "622510490430",
-  appId: "1:622510490430:web:72838df183aa78793be26f",
-  measurementId: "G-4SSGDH4WXR",
+  apiKey: 'AIzaSyCU1CkfC-zag_0jTDr6wdwkbGtKJuoDWms',
+  authDomain: 'homies-d82f4.firebaseapp.com',
+  projectId: 'homies-d82f4',
+  storageBucket: 'homies-d82f4.appspot.com',
+  messagingSenderId: '622510490430',
+  appId: '1:622510490430:web:72838df183aa78793be26f',
+  measurementId: 'G-4SSGDH4WXR'
 };
 
 const firebase = Firebase.initializeApp(config);
 
 const { FieldValue } = Firebase.firestore;
-
-// seedDatabase(firebase);
 
 export { firebase, FieldValue };
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
 
 export default function Login() {
-  return <p>I am a login page</p>;
+  const history = useHistory();
+  const { firbase } = useContext(FirebaseContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const isInvalid = password === "" || email === "";
+
+  const handleLogin = () => {};
+
+  useEffect(() => {
+    document.title = "login - HOMIES";
+  }, []);
+
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="phones with logo"
+          className="max-w-full"
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <h1 className="fle justify-center w-full">
+          <img src="/images/logo.png" alt="logo" className="mt-2 w-6/12 mb-5" />
+        </h1>
+
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            type="text"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            placeholder="Email address"
+            aria-label="Enter your email address"
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+            placeholder="Password"
+            aria-label="Enter your password"
+            onChange={(e) => {
+              console.log(e.target.value);
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
